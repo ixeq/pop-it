c694b81f53e843ae435bc1942ab863684ce05d40
Александр Матыскин <gr404_maaiv@lab-407-7.tomtit-tomsk.ru>
2023-04-18 13:28:04 +0700

    pop-it-complit

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,22 +2,49 @@
 
 namespace Controller;
 
+use Model\Post;
 use Src\View;
+use Src\Request;
+use Model\User;
+use Src\Auth\Auth;
 
 class Site
 {
+    public function index(Request $request): string
+    {
+        $posts = Post::where('id', $request->id)->get();
+        return (new View())->render('site.post', ['posts' => $posts]);
+    }
+
 
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
 
-    public function index(): string
+    public function signup(Request $request): string
     {
-        $view = new View();
-        return $view->render('site.hello', ['message' => 'index working']);
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        return new View('site.signup');
     }
 
+    public function login(Request $request): string
+    {
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
+    }
 
 
 }
